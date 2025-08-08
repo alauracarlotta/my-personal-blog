7b914134649812e7c7d690e84e194babc66fabdd
alauracarlotta <alauracarlotta@gmail.com>
2025-08-08 18:00:46 -0300

    feat(layout): add header and footer components

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+"use client";
+export const Footer = () => {
+	console.log("Lado de cá");
+
+	return (
+		<div className="bg-red-800 font-italic ">
+			<h1>Footer</h1>
+		</div>
+	);
+};
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+"use client";
+export const Header = () => {
+	console.log("Lado de cá");
+
+	return (
+		<div className="bg-blue-950 italic">
+			<h1>meu header</h1>
+		</div>
+	);
+};
