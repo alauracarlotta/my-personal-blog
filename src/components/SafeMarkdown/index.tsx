146344c47be8f3f46a212f3ef84117a487c10fd0
alauracarlotta <alauracarlotta@gmail.com>
2025-08-14 20:45:01 -0300

    feat(/components): create SafeMarkdown component and its style

---
+++ src/components/SafeMarkdown/index.tsx
@@ -0,0 +1,50 @@
+import clsx from 'clsx';
+import ReactMarkdown from 'react-markdown';
+import rehypeSanitize from 'rehype-sanitize';
+import remarkGfm from 'remark-gfm';
+
+type SafeMarkdownProps = {
+	markdown: string;
+};
+
+export const SafeMarkdown = ({ markdown }: SafeMarkdownProps) => {
+	return (
+		<div
+			className={clsx(
+				[
+					'prose',
+					'prose-slate',
+					'prose-a:transition',
+					'prose-a:no-underline',
+					'prose-a:text-blue-500',
+					'prose-a:hover:text-blue-700',
+					'prose-a:hover:underline',
+					'prose-img:mx-auto',
+					'w-full',
+					'max-w-none',
+					'overflow-hidden',
+				],
+				['md:prose-lg'],
+			)}
+		>
+			{/* <div className='my-component'> */}
+			<ReactMarkdown
+				rehypePlugins={[rehypeSanitize]}
+				remarkPlugins={[remarkGfm]}
+				components={{
+					table: ({ node, ...props }) => {
+						if (!node?.children) return '';
+
+						return (
+							<div className='overflow-x-auto'>
+								<table className='w-full min-w-[600px]' {...props} />
+							</div>
+						);
+					},
+				}}
+			>
+				{markdown}
+			</ReactMarkdown>
+		</div>
+	);
+};
--- src/components/SinglePost/index.tsx
+++ src/components/SinglePost/index.tsx
@@ -1,6 +1,7 @@
 import { findPostBySlugCached } from '@/libs/post/queries';
 import Image from 'next/image';
 import { formatDatetime } from '@/utils/_format-datetime';
+import { SafeMarkdown } from '../SafeMarkdown';
 
 type SinglePostProps = {
 	slug: string;
@@ -25,22 +26,27 @@ export const SinglePost = async ({ slug }: SinglePostProps) => {
 				typePost='postList'
 				as='h1'
 			/> */}
-			<article>
-				<header>
-					<div>{formatDatetime(post.createdAt)}</div>
-					<div>Materia by {post.author}</div>
+			<article className='mb-12'>
+				<header className='mb-6'>
 					<Image
+						className='rounded-2xl mb-2'
 						src={post.coverImageUrl}
 						alt={post.title}
 						width={1200}
 						height={720}
 					/>
-					<div>{post.title}</div>
+					<div className='flex italic gap-2 ml-2 mb-4 text-sm'>
+						<span>{formatDatetime(post.createdAt)}</span>
+						<span> | </span>
+						<span>
+							Materia by <span className='font-bold'>{post.author}</span>
+						</span>
+					</div>
+					<div className='text-4xl/tight py-6 font-bold'>{post.title}</div>
 				</header>
 
 				<main>
-					<div>{post.excerpt}</div>
-					<p>{post.content}</p>
+					<SafeMarkdown markdown={post.content} />
 				</main>
 			</article>
 		</>
