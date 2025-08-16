6c8f1eea7996ca93b090546aa31a58ea0a9ece2d
alauracarlotta <alauracarlotta@gmail.com>
2025-08-16 16:28:26 -0300

    feat(next.config): add generateStaticParams next pattern to render static page application

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,6 +1,9 @@
 import { SinglePost } from '@/components/SinglePost';
 import { SpinLoader } from '@/components/SpinLoader';
-import { findPostBySlugCached } from '@/libs/post/queries';
+import {
+	findAllPublicPostsCached,
+	findPostBySlugCached,
+} from '@/libs/post/queries';
 import { Metadata } from 'next';
 import { Suspense } from 'react';
 
@@ -17,6 +20,18 @@ export const generateMetadata = async ({
 		title: post.title,
 		description: post.excerpt,
 	};
+};
+
+export const generateStaticParams = async () => {
+	const posts = await findAllPublicPostsCached();
+	const params = posts.map((post) => {
+		return {
+			slug: post.slug,
+		};
+	});
+	console.log(params);
+
+	return params;
 };
 
 export default async function PostSlugPage({ params }: PostSlugPageProps) {
