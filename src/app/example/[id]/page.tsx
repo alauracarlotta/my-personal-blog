0f0aeaa9770dd5ed656c4add00e6d4d37b282ff9
alauracarlotta <alauracarlotta@gmail.com>
2025-08-26 19:26:03 -0300

    chore(/example): introduce static, dynamic and ssg context page

--- src/app/example/[id]/page.tsx
+++ src/app/example/[id]/page.tsx
@@ -1,13 +1,36 @@
 import { formatHour } from '@/utils/format-datetime';
+import { revalidatePath, revalidateTag } from 'next/cache';
 
-export default async function ExampleDynamicPage() {
+revalidatePath('/example/1');
+revalidateTag('example-1');
+
+export const revalidate = 10;
+export const dynamic = 'force-static';
+/* export const generateStaticParams = async () => {
+	return [];
+}; */
+
+// export const dynamicParams = true;
+
+/* export const generateStaticParams = async () => {
+	return [{ id: '1' }, { id: '2' }];
+}; */
+
+export default async function ExampleDynamicPage({
+	params,
+}: {
+	params: Promise<{ id: string }>;
+}) {
+	const { id } = await params;
 	const hour = formatHour(Date.now());
 	return (
 		<>
 			<main>
 				<div>Estamos em example</div>
 				<div>Veja só</div>
-				<div>{hour}</div>
+				<div>
+					{hour} (ID: {id})
+				</div>
 			</main>
 		</>
 	);
